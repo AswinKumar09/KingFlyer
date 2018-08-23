d5ebcf4bae072e64fb055ec13bf94f4c745cfe20
AswinKumar09 <aswi14109.it@rmkec.ac.in>
2018-08-23 09:05:43 +0530

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
 
   {path:"home", component:HomeComponent}, 
   {path:"", redirectTo:"/home", pathMatch:"full"},
-  {path:"search/:from/:to/:dt",component:SearchDetailComponent}
+  {path:"search",component:SearchDetailComponent}
 ];
 
 
